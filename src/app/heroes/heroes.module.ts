0376a5b98b5b1d857fe0e3e4f65f5bc5eb4f4846
Danny Armijos <daniel2667@hotmail.es>
2024-04-07 18:47:16 +0200

    creacion de interfaz y de json-server

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,6 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     NewPageComponent,
     SearchPageComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+type listNavigation = {
+  label: string;
+  icon: string;
+  url: string;
+};
 
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LayoutPageComponent {
-
+  sidevarItems: listNavigation[]=[
+    {label: 'Listado', icon:'label', url:'./list'},
+    {label: 'Añadir', icon:'add', url:'./new-hero'},
+    {label: 'Buscar', icon:'search', url:'./search'}
+  ];
 }
