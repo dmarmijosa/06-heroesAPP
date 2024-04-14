049cc6296557194f268986d52f498e5df82c1dc4
Danny Armijos <daniel2667@hotmail.es>
2024-04-14 14:01:29 +0200

    build prod

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -9,14 +9,12 @@ import { Router } from '@angular/router';
   styles: ``,
 })
 export class LoginPageComponent {
-  user = new FormControl('');
   constructor(
     private authService: AuthService,
     private router: Router,
   ) {}
 
   onLogin(): void {
-    console.log(this.user.value);
     this.authService
       .login('daniel2667@hotmail.es', '12251619')
       .subscribe((user) => {
